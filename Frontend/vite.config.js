f70519ad635ca9d5d73d5c006f1a0121cd166b4b
Samarth Jaiswal <samarthjaiswal369@Samarths-MacBook-Pro.local>
2026-05-28 02:48:16 +0530

    Fix Vercel base path

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Roadwatch-practice-/',
+  base: "/",
   plugins: [react(), tailwindcss()],
 })
