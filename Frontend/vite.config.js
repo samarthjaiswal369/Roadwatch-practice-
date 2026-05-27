37e785325c6a88b51e96510546eaa1c40a0bb5dc
Samarth Jaiswal <samarthjaiswal369@Samarths-MacBook-Pro.local>
2026-05-28 02:54:45 +0530

    Support both Vercel and GitHub Pages

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isVercel = process.env.VERCEL;
+
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: isVercel ? "/" : "/Roadwatch-practice-/",
 })
